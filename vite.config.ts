a722c9ec4fea44ad8ed60f5bf2123968a113133a
Mooncake Sugar <Tsukina7mochi@gmail.com>
2026-02-03 22:46:18 +0900

    build: setup environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
+import preactRefresh from "@prefresh/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vite.dev/config/
 export default defineConfig(async () => ({
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
+  // prevent Vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+  // tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
@@ -23,8 +21,13 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
+  esbuild: {
+    jsxFactory: "h",
+    jsxFragment: "Fragment",
+    jsxImportSource: "preact",
+  },
+  plugins: [tailwindcss(), preactRefresh()],
 }));
